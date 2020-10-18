748de951f40472260232fd34ed52261816a68a3b
Roshan Acharya <acharyaroshan2357@gmail.com>
2020-10-18 21:27:15 +0545

    added name for contents as it is compul in ulka6

--- ulka-config.js
+++ ulka-config.js
@@ -1,18 +1,19 @@
 module.exports = {
-    siteMetaData: {
-      title: "hacktoberfest-2020",
-      description: "hacktoberfest-2020",
-      domain: "https://426codershacktoberfest2020.netlify.app",
+  siteMetaData: {
+    title: 'hacktoberfest-2020',
+    description: 'hacktoberfest-2020',
+    domain: 'https://426codershacktoberfest2020.netlify.app',
+  },
+  buildPath: 'build',
+  pagesPath: 'pages',
+  templatesPath: 'templates',
+  plugins: [],
+  contents: [
+    {
+      path: 'contributors',
+      generatePath: '.',
+      template: 'hack.ulka',
+      name: 'contributors',
     },
-    buildPath: "build",
-    pagesPath: "pages",
-    templatesPath: "templates",
-    plugins: [],
-    contents: [
-      {
-        path: "contributors",
-        generatePath: ".",
-        template: "hack.ulka",
-      },
-    ],
-  };
+  ],
+}
